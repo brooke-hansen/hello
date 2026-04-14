d4aa96e70da0e0e00131b9c3f093ea85751a9ed4
brooke-hansen <ij4689bm@go.minnstate.edu>
2026-04-14 10:06:41 -0500

    4/14 pt 2

--- week12practice/file_parts2- writing.py
+++ week12practice/file_parts2- writing.py
@@ -8,14 +8,29 @@ my_file.write('hello world')
 
 family_file = open('family.txt', 'w')
 family_file.write("first last age\n")
-family_file.write('Amanda, 47\n')
-family_file.write('Bryon, 47\n')
-family_file.write('Britney, 24\n')
-family_file.write('Bryon, 22\n')
+family_file.write('Amanda, Connors, 47\n')
+family_file.write('Bryon, Hansen, 47\n')
+family_file.write('Brooke, Hansen, 26\n')
+family_file.write('Britney, Hansen, 24\n')
+family_file.write('Bryon, Hansen, 22\n')
 
 family_file.close()
 
+family_file = open('family.txt', 'r')
 
+total = 0
+count = 0 
 
+for line in family_file.readlines()[1:]:
+    line_data = line.split(",")
+    #print(line_data)
+    first_name = line_data[0]
+    last_name = line_data[1]
+    age = int(line_data[2])
+    count += 1
+    total += age
+
+mean = total / count
+print(mean)
 
 
